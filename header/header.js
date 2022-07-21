59d7e666834019b4ac086d61765e419085a2d2e4
atalkrpandey <105963524+atalkrpandey@users.noreply.github.com>
2022-07-21 16:20:03 +0530

    Add files via upload
    Add file

---
+++ header/header.js
@@ -0,0 +1,21 @@
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+document.querySelector("#gift").addEventListener("click",function(){
+  window.location.href="gift.html";
+})
