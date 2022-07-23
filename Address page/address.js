a5c88a1cad69915cb8e8829d9001a5b6942818cf
Rahul Somase <105917553+Rahulsomase@users.noreply.github.com>
2022-07-23 09:52:22 +0530

    Update address.js

--- Address page/address.js
+++ Address page/address.js
@@ -1,3 +1,4 @@
+
 document.querySelector("form").addEventListener("submit" , myAddress)
 let userAdd = JSON.parse(localStorage.getItem("UserAddress")) || [];
 
@@ -15,5 +16,42 @@ function myAddress(){
   };   
  userAdd.push(data)
  localStorage.setItem("UserAddress" ,JSON.stringify(userAdd))
-window.open("orderSummary.html");
+}
+
+
+let arrFromLS =JSON.parse (localStorage.getItem("UserAddress"));
+  
+displayUser(arrFromLS)
+
+function  displayUser(arrFromLS){
+
+ document.querySelector("#addDet").innerHTML=""
+
+  arrFromLS.forEach(function(elem,dlt){
+
+      let box = document.createElement("div")
+      box.setAttribute("class" , "result")
+
+      let para1 = document.createElement("p")
+      para1.innerText = elem.fname
+      para1.style.fontSize="16px"
+
+      let para2 = document.createElement("p")
+      para2.innerText = elem.lname
+      para2.style.lineHeight="0px"
+
+      let para3 = document.createElement("p")
+      para3.innerText = elem.adds
+      let para4 = document.createElement("p")
+      para4.innerText = elem.cty
+
+      let para5 = document.createElement("p")
+      para5.innerText = elem.pin
+
+      let para6 = document.createElement("p")
+      para6.innerText = elem.mob
+
+      box.append(para1,para2,para3,para4,para5,para6)
+      document.querySelector("#addDet").append(box)
+  })
 }
